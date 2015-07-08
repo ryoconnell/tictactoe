203c840b1b2f9260f2dd8d0ae610a153b9a4484f
ryoconnell <ry.oconnell@gmail.com>
2015-07-08 18:38:07 -0400

    added jquery source link, console log to tope of js

--- code/javascript.js
+++ code/javascript.js
@@ -1,15 +1,20 @@
-use 'select'
+// A $( document ).ready() block.
+$( document ).ready(function() {
+console.log( "ready!" );
 
 function setname(id)
-    {
-        if(id == "eks")
-        {
-          player1 = prompt("Please enter your name", "Tom Brady");
-    document.getElementById("p1").innerHTML = player1;
-        }
-        else if(id == "oh")
-        {
-          player2 = prompt("Please enter your name, player 2", "Peyton Manning");
-          document.getElementById("p2").innerHTML = player2;
-        }
-    }
+{
+if(id == "eks")
+{
+player1 = prompt("Please enter your name", "Tom Brady");
+document.getElementById("p1").innerHTML = player1;
+}
+else if(id == "oh")
+{
+player2 = prompt("Please enter your name, player 2", "Peyton Manning");
+document.getElementById("p2").innerHTML = player2;
+}
+}
+
+}); // end document ready function
+
